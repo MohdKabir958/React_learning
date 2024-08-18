2cb4e412059eb8ea1e1989125a3cb5641f251dc6
mohdkabir <sarwarmohd470@gmail.com>
2024-08-18 15:39:19 +0530

    added Chai function and understand componenets

---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,10 @@
+function Chai() {
+    return (
+        <>
+            <h1>Chai or React with Vite</h1>
+            <p>Added Chai function </p>
+            <p>learn components</p>
+        </>
+    )
+}
+export default Chai
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Chai from './Chai.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+<StrictMode>  
     <App />
-  </StrictMode>,
+    <Chai/>
+</StrictMode>
+/*
+fragmentation in react
+<></>
+*/
 )
