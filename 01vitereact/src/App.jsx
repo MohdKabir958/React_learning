5b799b98d2f0fc15266bc2e99d043bdce5f80fa1
mohdkabir <sarwarmohd470@gmail.com>
2024-08-18 14:48:23 +0530

    seen react video#2

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1> Chai or React with Vite | Mohd Kabir</h1>
+  )
+}
+
+export default App
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 chai or React
+npx :- node package executer
+npx create-react-app 01basicreact
