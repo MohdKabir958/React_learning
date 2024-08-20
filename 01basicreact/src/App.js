63260140643454f1dbd61bd2e26edf64e6a0c3f3
mohdkabir <sarwarmohd470@gmail.com>
2024-08-20 11:23:43 +0530

    learn components video#3 completed

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>chai or React |M.Kabir</h1>
+  )
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h1> A component with React |M.Kabir</h1>
+    )
+}
+export default Chai
